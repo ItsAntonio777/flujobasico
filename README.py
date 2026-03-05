004b78f2657ca5da7d9c376f5e08b90681549b52
ItsAntonio777 <luisvideostj@gmail.com>
2026-03-05 11:39:47 -0800

    readme

--- README.py
+++ README.py
@@ -1,6 +1,10 @@
-# Práctica 1 - Flujo Básico de git y github Este es mi segundo repositorio en Github
+# Práctica 1 - Flujo Básico de git y github 
+Este es mi segundo repositorio en Github
 Sera sobre un flujo basico que guardara y aplicara cambios
 ```sh
 git status
-git add . git status
+git add .
+git status
 git commit -m "Descripcion del cambio que hiciste"
+git push
+```
